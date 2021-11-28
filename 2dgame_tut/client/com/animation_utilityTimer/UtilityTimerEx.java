94775f6a6a6f0981f38b33a5dcc5a05b6fb144f2
stanley chilton - surface <stanleychilton@live.com>
2021-11-28 21:19:02 +1300

    adding new screenfiles

--- 2dgame_tut/client/com/animation_utilityTimer/UtilityTimerEx.java
+++ 2dgame_tut/client/com/animation_utilityTimer/UtilityTimerEx.java
@@ -1,5 +1,32 @@
 package com.animation_utilityTimer;
 
-public class UtilityTimerEx {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
+public class UtilityTimerEx extends JFrame {
+	
+	public UtilityTimerEx() {
+	
+		initUI();
+	}
+	
+	private void initUI() {
+		
+		add(new Board());
+		
+		setResizable(false);
+		pack();
+		
+		setTitle("star");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+			JFrame ex = new UtilityTimerEx();
+			ex.setVisible(true);
+		});
+	}
 }
